9ce4cd2dd2b98792d246123cccc09c03ac010f23
Sergey Sova <i.am@lestad.net>
2016-10-12 18:35:57 +0300

    Add require hook

--- bin/hooks.js
+++ bin/hooks.js
@@ -1,3 +1,9 @@
+const { resolve } = require('path')
+const { Module } = require('module')
+
+process.env.NODE_PATH = resolve(__dirname, '..')
+Module._initPaths()
+
 require('babel-register')({
   ignore: /node_modules/,
 })
--- bin/start.js
+++ bin/start.js
@@ -1,5 +1,5 @@
 
 import fs from 'fs'
+import A from 'src/example'
 
-
-console.log(fs.readFileSync)
+console.log(fs.readFileSync, A)

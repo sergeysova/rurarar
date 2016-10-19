fef2b834aedcf939d33f133c315c477e66149dcc
Sergey Sova <i.am@lestad.net>
2016-10-19 20:01:10 +0300

    Add link

--- src/client.js
+++ src/client.js
@@ -6,3 +6,6 @@ import ReactDOM from 'react-dom'
 import App from './app'
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('wbpp'))
+
+// HOT-reloading
+// https://github.com/glenjamin/ultimate-hot-reloading-example/blob/master/server.js#L33-L54

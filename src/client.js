4665f523235873c1a2156548517275be14c1d8f1
Sergey Sova <i.am@lestad.net>
2016-12-14 02:58:08 +0300

    Refactor scripts

--- src/client.js
+++ src/client.js
@@ -6,6 +6,3 @@ import ReactDOM from 'react-dom'
 import App from './app'
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('wbpp'))
-
-// HOT-reloading
-// https://github.com/glenjamin/ultimate-hot-reloading-example/blob/master/server.js#L33-L54

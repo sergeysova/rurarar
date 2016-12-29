53d7617a53e1fb081f9f1db15fa91b03b144697d
Sergey Sova <i.am@lestad.net>
2016-12-29 08:06:53 +0300

    Update deps

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,15 @@ import { resolve } from 'path'
 import Express from 'express'
 import debug from 'debug'
 import morgan from 'morgan'
-import { watch } from 'chokidar'
-
-import config from '../../webpack/dev.config'
 
 
 /* global __DEVELOPMENT__ */
 
 const LOG = debug('APP:SERVER')
 const app = Express()
+let config = {
+  port: process.env.PORT || 4500,
+}
 
 app.use(morgan('combined'))
 
@@ -26,6 +26,9 @@ function clearDependencies(regexp) {
 
 if (__DEVELOPMENT__) {
   LOG('Enabled development mode.')
+
+  config = require('../../webpack/dev.config')
+  const { watch } = require('chokidar')
 
   const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')

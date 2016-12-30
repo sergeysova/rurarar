875b02ae9c4d05fb8e82099949b48ed10b96ec9f
Sergey Sova <i.am@lestad.net>
2016-12-30 06:06:38 +0300

    Try to fix deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,10 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 9012,
       },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 9012,
+      },
     },
   ],
   deploy: {
